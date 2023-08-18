7a2a528ebe702075c6a872483d49d32ac54ec0cc
shubashdutta <141384565+shubashdutta@users.noreply.github.com>
2023-08-18 12:38:22 +0545

    Update main.go

--- main.go
+++ main.go
@@ -14,14 +14,14 @@ func main() {
 
 	// Create a new CORS handler with the desired options
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOrigins:   []string{"*"}, // Allowing all origins
 		AllowCredentials: true,
 	})
 
 	// Wrap the router with the CORS handler
 	handler := c.Handler(r)
 
-	port := "8080" // Change the port to your desired value
+	port := "8081" // Change the port to your desired value
 
 	fmt.Printf("Server is ready and running on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, handler))
